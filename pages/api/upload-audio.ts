70eebc4c8eea2c8f7e2a68502ea5b61391f5df82
perplexedyawdie <sunkwenga@gmail.com>
2023-02-20 11:53:09 -0500

    added db connection

--- pages/api/upload-audio.ts
+++ pages/api/upload-audio.ts
@@ -4,7 +4,8 @@ import { S3Client, PutObjectCommand, GetObjectCommand  } from '@aws-sdk/client-s
 import fs from 'fs'
 import { v4 as uuidv4 } from 'uuid';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-
+import ampq from 'amqplib'
+import { dbConnection } from '@/config/db';
 
 const form = formidable({ multiples: false })
 
@@ -20,11 +21,16 @@ export default async function handler(
     }
 
     try {
+        const db = dbConnection();
         const s3Client = new S3Client({ 
             region: process.env.LINODE_REGION,
             endpoint: process.env.LINODE_ENDPOINT,
             
          });
+         const queue = 'tasks'
+         const conn = await ampq.connect("amqp://localhost:5672")
+         const ch1 = await conn.createChannel();
+         await ch1.assertQueue(queue);
         let fileId = uuidv4();
         const fileContent: {
             buff: Buffer,
@@ -54,7 +60,15 @@ export default async function handler(
             Bucket: process.env.LINODE_BUCKET,
         });
         const url = await getSignedUrl(s3Client, command, { expiresIn: 86400 });
-
+        const queueMsg = {
+            id: fileId,
+            url
+        }
+        ch1.sendToQueue(queue, Buffer.from(JSON.stringify(queueMsg)));
+       
+            
+        const queryResult = await db.query('CALL init_file(?, ?)', [fileId, (new Date).toISOString()])
+        console.log(queryResult)
         res.status(200).send({ message: 'ok' })
     } catch (err) {
         console.error(err)
